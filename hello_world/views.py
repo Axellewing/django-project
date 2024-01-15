e23f2a45bf776be2403ecefb251b905e512d29b3
Axel Lewing <axel_lewing@hotmail.com>
2024-01-15 16:42:21 +0000

    adding a about me page to about app

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Hello, world")
+
+def about_me(request):
+    return HttpResponse("This would be the about page")    
